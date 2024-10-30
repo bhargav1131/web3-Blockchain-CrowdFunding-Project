ec126f81756fb566ee454c93d181d9c3df2ec444
Bhargav Pratim Sharma <89238556+bhargav1131@users.noreply.github.com>
2024-10-30 09:10:59 +0530

    configure hardhat, add rpc for sepolia test net

--- crowdfunding/hardhat.config.js
+++ crowdfunding/hardhat.config.js
@@ -5,6 +5,14 @@ module.exports = {
   zksolc: {
     version: "1.3.9",
     compilerSource: "binary",
+    defaultNetwork: 'sepolia',
+    networks:{
+      hardhat: {},
+      sepolia:{
+        url: 'https://rpc.sepolia.org/',
+        accounts:[`0x${process.env.PRIVATE_KEY}`]
+      }
+    }
     settings: {
       optimizer: {
         enabled: true,
