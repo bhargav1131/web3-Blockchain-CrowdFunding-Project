d8a296b6a0344aa41eba5bb3b45a8ffc4aef228d
Bhargav Pratim Sharma <89238556+bhargav1131@users.noreply.github.com>
2024-11-01 11:45:47 +0530

    import the pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,8 @@
 import React from 'react';
 import {Route, Routes} from 'react-router-dom';
 
+import {CampaignDetails, CreateCampaign, Home, Profile} from './pages';
+
 const WelcomeComponent: React.FC = () => {
   return (
     <div className = "relative sm:-8 p-4 bg-[#13131a] min-h-screen flex flex-row">
