bb4539609917af011b6c1822522043d06960c3ec
Bhargav Pratim Sharma <89238556+bhargav1131@users.noreply.github.com>
2024-11-01 11:51:25 +0530

    import from components

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,16 +3,19 @@ import React from 'react';
 import {Route, Routes} from 'react-router-dom';
 
 import {CampaignDetails, CreateCampaign, Home, Profile} from './pages';
+import {Navbar, Sidebar} from './components';
 
 const WelcomeComponent: React.FC = () => {
   return (
     <div className = "relative sm:-8 p-4 bg-[#13131a] min-h-screen flex flex-row">
       <div className="sm:flex hidden mr-10 relative">
-          Sidebar
+          <Sidebar/>
+
       </div>
 
       <div className="flex-1 max-sm:w-full max-w-[1280px] mx-auto sm:pr-5">
-          Navbar
+          <Navbar/>
+
           <Routes>
             <Route path="/" element={<Home />} />
           </Routes>
