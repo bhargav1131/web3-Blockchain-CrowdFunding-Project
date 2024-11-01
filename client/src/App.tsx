0e583d14c035ac29d96d2ef5a6775a841b680f13
Bhargav Pratim Sharma <89238556+bhargav1131@users.noreply.github.com>
2024-11-01 11:40:58 +0530

    add route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,22 @@
 // App.tsx
 import React from 'react';
+import {Route, Routes} from 'react-router-dom';
 
 const WelcomeComponent: React.FC = () => {
   return (
-    <div>
-      Welcome to Web3.. Hello
+    <div className = "relative sm:-8 p-4 bg-[#13131a] min-h-screen flex flex-row">
+      <div className="sm:flex hidden mr-10 relative">
+          Sidebar
+      </div>
+
+      <div className="flex-1 max-sm:w-full max-w-[1280px] mx-auto sm:pr-5">
+          Navbar
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+      </div>
     </div>
   );
 };
 
-export default WelcomeComponent; // Make sure to export WelcomeComponent as default
+export default WelcomeComponent; 
