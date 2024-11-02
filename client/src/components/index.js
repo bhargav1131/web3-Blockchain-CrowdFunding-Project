0c5be8aa12f9b034db6997675eb6d8cac88abe97
Bhargav Pratim Sharma <89238556+bhargav1131@users.noreply.github.com>
2024-11-02 20:31:45 +0530

    export form field

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,4 +1,5 @@
 export { default as Sidebar } from './Sidebar';
 export { default as Navbar } from './Navbar';
 export { default as CustomButton } from './CustomButton';
+export { default as FormField } from './FormField';
 export const contractAddress = process.env.REACT_APP_CONTRACT_ADDRESS; // Updated to use environment variable
