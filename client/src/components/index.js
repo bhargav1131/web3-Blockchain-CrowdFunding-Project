41042debf787038615d7cbbfc00f287c1b99c066
Bhargav Pratim Sharma <89238556+bhargav1131@users.noreply.github.com>
2024-11-02 16:44:46 +0530

    ...

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,3 +1,4 @@
 export { default as Sidebar } from './Sidebar';
 export { default as Navbar } from './Navbar';
 export { default as CustomButton } from './CustomButton';
+export const contractAddress = process.env.REACT_APP_CONTRACT_ADDRESS; // Updated to use environment variable
