7d132fa01d45ce4c7210a9b1730058c22903eb34
Bhargav Pratim Sharma <89238556+bhargav1131@users.noreply.github.com>
2024-11-02 16:44:16 +0530

    export the contract address

--- client/src/constants/index.js
+++ client/src/constants/index.js
@@ -1,4 +1,6 @@
 import { createCampaign, dashboard, logout, payment, profile, withdraw } from '../assets';
+export const contractAddress = process.env.REACT_APP_CONTRACT_ADDRESS; // Updated to use environment variable
+
 
 export const navlinks = [
   {
