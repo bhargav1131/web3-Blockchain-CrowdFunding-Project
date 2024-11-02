17d7e43d3b2245e2687afc3f656a7a36ec57a7b1
Bhargav Pratim Sharma <89238556+bhargav1131@users.noreply.github.com>
2024-11-02 20:52:36 +0530

    export the  loader

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -2,4 +2,5 @@ export { default as Sidebar } from './Sidebar';
 export { default as Navbar } from './Navbar';
 export { default as CustomButton } from './CustomButton';
 export { default as FormField } from './FormField';
+export { default as Loader } from './Loader';
 export const contractAddress = process.env.REACT_APP_CONTRACT_ADDRESS; // Updated to use environment variable
