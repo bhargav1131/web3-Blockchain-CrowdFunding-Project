b75c5b93d6ae64e8acc22923845a985c0bc19c51
Bhargav Pratim Sharma <89238556+bhargav1131@users.noreply.github.com>
2024-11-02 16:45:35 +0530

    fix issue

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -11,7 +11,7 @@ const Navbar = () => {
   const navigate = useNavigate();
   const [isActive, setIsActive] = useState('dashboard');
   const [toggleDrawer, setToggleDrawer] = useState(false);
-  const { connect, address } = useStateContext();
+  const { connect, address} = useStateContext();
 
   return (
     <div className="flex md:flex-row flex-col-reverse justify-between mb-[35px] gap-6">
